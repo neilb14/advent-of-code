31148a352a0b8106e7aeaf1327753e46a2d605a2
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-12-09 13:09:15 -0700

    solved day 3 problem 2

--- day3/solution.py
+++ day3/solution.py
@@ -6,6 +6,26 @@ def increment(x):
 def decrement(x):
     return x-1
 
+def move_right(matrix, i, j):
+    if(matrix[i+1][j-1] == 0):
+        return i+1, j, move_up
+    return i+1, j, move_right
+
+def move_up(matrix, i, j):
+    if(matrix[i-1][j-1] == 0):
+        return i, j-1, move_left
+    return i, j-1, move_up
+
+def move_left(matrix, i, j):
+    if(matrix[i-1][j+1] == 0):
+        return i-1, j, move_down
+    return i-1, j, move_left
+
+def move_down(matrix, i, j):
+    if(matrix[i+1][j+1] == 0):
+        return i, j+1, move_right
+    return i, j+1, move_down
+
 def calculate_square(size):
     results = []
     max_r = size - 1
@@ -24,7 +44,7 @@ def calculate_square(size):
 def distance(input):
     size = 1
     square = [0]
-    result = 0
+    result = 1
     position = 0
     i=1
     while(i <= input): 
@@ -37,5 +57,35 @@ def distance(input):
             position = 0
     return result
 
+def calculate_sum(matrix, x, y):
+    result = 0
+    for i in range(x-1,x+2):
+        for j in range(y-1, y+2):
+            if(i == x and j == y):
+                continue
+            result += matrix[i][j]
+    return result
+
+def move(matrix, i, j):
+    if(matrix[i-1][j] == 0):
+        return i-1, j
+    if(matrix[i][j-1] == 0):
+        return i-1, j
+    
+
+def stress_of_square(n):
+    matrix = [[0 for x in range(1000)] for y in range(1000)]
+    center = 500
+    i,j = center, center
+    matrix[i][j] = 1
+    result = 1
+    move = move_right
+    while(result < n):
+        i,j,move = move(matrix, i, j)
+        result = calculate_sum(matrix, i, j)
+        matrix[i][j] = result
+    return result
+
 if __name__ == '__main__':
-    print(distance(int(sys.argv[1])))
+    n = int(sys.argv[1])
+    print(distance(n), ' ', stress_of_square(n))
--- day3/test.py
+++ day3/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import distance, calculate_square
+from solution import distance, calculate_square, stress_of_square, calculate_sum
 
 class TestCaptcha(unittest.TestCase):
 
@@ -21,6 +21,20 @@ class TestCaptcha(unittest.TestCase):
         self.assertEqual(distance(12), 3)
         self.assertEqual(distance(23), 2)
         self.assertEqual(distance(1024), 31)
-    
+
+    def test_calculate_sum(self):
+        square = [[1 for x in range(3)] for y in range(3)]
+        self.assertEqual(calculate_sum(square, 1, 1), 8)
+
+    def test_stress_of_square(self):
+        self.assertEqual(stress_of_square(1), 1)
+        self.assertEqual(stress_of_square(2), 2)
+        self.assertEqual(stress_of_square(6), 10)
+        self.assertEqual(stress_of_square(10), 10)
+        self.assertEqual(stress_of_square(11), 11)
+        self.assertEqual(stress_of_square(12), 23)
+        self.assertEqual(stress_of_square(58), 59)
+        self.assertEqual(stress_of_square(750), 806)
+        
 if __name__ == '__main__':
     unittest.main()
