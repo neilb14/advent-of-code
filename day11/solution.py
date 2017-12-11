896a72ae1e91a8c47134acb84a3e55ed0051ab8e
Neil Bourgeois <bourgeoisn@Neil-Bourgeoiss-MacBook-Pro.local>
2017-12-11 15:36:24 -0700

    solved day11 problem 1

--- day11/solution.py
+++ day11/solution.py
@@ -1,31 +1,36 @@
 import sys
 
-
+def cost(path):
+    result = 0
+    for k in path:
+        result += path[k]
+    return result
 
 def walk(input):
     path = {'n':0,'ne':0,'nw':0, 's':0, 'sw':0, 'se': 0}
-    result = 0
     for step in input.split(','):        
         path[step] +=1
-    for pair in [['ne','nw','n'],['se','sw','s']]:
-        left, right = pair[0],pair[1]
-        n = min(path[left],path[right])
-        path[left] -=n
-        path[right] -=n 
-        path[pair[2]] += n
-    for pair in [['ne','s','se'], ['nw','s','sw'], ['se','n','ne'], ['sw','n','nw']]:
-        diagonal,vertical,to = pair[0],pair[1],pair[2]
-        n = min(path[diagonal],path[vertical])
-        path[diagonal] -=n
-        path[vertical] -=n
-        path[to] +=n    
-    for pair in [['n','s'], ['ne','sw'], ['nw','ne']]:
-        left, right = pair[0],pair[1]
-        n = min(path[left],path[right])
-        path[left] -=n
-        path[right] -=n    
-    for k in path:
-        result += path[k]
+    last_result, result = cost(path)+1, cost(path)
+    while(result < last_result):
+        for pair in [['ne','nw','n'],['se','sw','s']]:
+            left, right = pair[0],pair[1]
+            n = min(path[left],path[right])
+            path[left] -=n
+            path[right] -=n 
+            path[pair[2]] += n
+        for pair in [['ne','s','se'], ['nw','s','sw'], ['se','n','ne'], ['sw','n','nw']]:
+            diagonal,vertical,to = pair[0],pair[1],pair[2]
+            n = min(path[diagonal],path[vertical])
+            path[diagonal] -=n
+            path[vertical] -=n
+            path[to] +=n    
+        for pair in [['n','s'], ['ne','sw'], ['nw','se']]:
+            left, right = pair[0],pair[1]
+            n = min(path[left],path[right])
+            path[left] -=n
+            path[right] -=n
+        last_result = result
+        result = cost(path)
     return result
 
 if __name__ == '__main__':
--- day11/test.py
+++ day11/test.py
@@ -6,8 +6,15 @@ class TestWalk(unittest.TestCase):
     def test_walk(self):
         self.assertEqual(walk('ne,ne,ne'), 3)
         self.assertEqual(walk('ne,ne,sw,sw'), 0)
+        self.assertEqual(walk('se,se,nw,nw'), 0)
         self.assertEqual(walk('ne,ne,s,s'), 2)
         self.assertEqual(walk('se,sw,se,sw,sw'), 3)
+        self.assertEqual(walk('ne,nw,ne,nw,nw'), 3)
+
+    def test_input(self):
+        self.assertEqual(walk('sw,s,se'), 2)
+        self.assertEqual(walk('sw,s,se,se,se,se,se,nw'),5)
+        #self.assertEqual(walk('sw,s,se,se,se,se,se,nw,ne,ne,ne,ne,ne,n,n,ne,n,n,n,n,n'), )
     
 if __name__ == '__main__':
     unittest.main()
