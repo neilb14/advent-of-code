48b87932a0b585195ecc0c23747912e22407e501
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-12-10 20:39:27 -0700

    solved day 10 problem 2

--- day10/solution.py
+++ day10/solution.py
@@ -15,12 +15,35 @@ def reverse(input, length, position):
 
 def checksum(input, lengths, position, skip):
     if(len(lengths) <= 0):
-        return input[0] * input[1]
+        return input, position, skip
     return checksum(reverse(input, lengths[0], position), lengths[1:], (position + lengths[0] + skip)%len(input), skip+1)
+
+def make_dense_hash(sparse_hash):
+    result = 0
+    for i in range(16):
+        result = sparse_hash[i] ^ result
+    return '%02x' % result
+
+def hash(input, lengths):
+    lengths_seq = [ord(c) for c in lengths] + [17,31,73,47,23]
+    result = input
+    position = 0
+    skip = 0
+    for i in range(64):
+        result, position, skip = checksum(result, lengths_seq, position, skip)
+    dense_hash = ''
+    for x in range(16):
+        start = x*16
+        end = (x+1)*16
+        dense_hash += make_dense_hash(result[start:end])
+    return dense_hash
 
 if __name__ == '__main__':
         input = []
+        key = ''
         for x in range(256):
             input.append(x)
+            key += chr(x)
         lengths = [46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204]
-        print(checksum(input, lengths, 0, 0))
+        checksum_result = checksum(input, lengths, 0, 0)
+        print(checksum_result[0]*checksum_result[1], ' ', hash(input, '46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204'))
--- day10/test.py
+++ day10/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import reverse, checksum
+from solution import reverse, checksum, hash, make_dense_hash
 
 class TestCaptcha(unittest.TestCase):
 
@@ -19,9 +19,22 @@ class TestCaptcha(unittest.TestCase):
         position = 1
         self.assertEqual(reverse([4,3,0,1,2], 5, position), [3,4,2,1,0])
 
+    def test_make_dense_hash(self):
+        self.assertEqual(make_dense_hash([65,27,9,1,4,3,40,50,91,7,6,0,2,5,68,22]), '%02x' % 64)
+
     def test_checksum(self):
         position = 0
-        self.assertEqual(checksum([0,1,2,3,4], [3,4,1,5], 0, 0), 12)
+        result, position, size = checksum([0,1,2,3,4], [3,4,1,5], 0, 0)
+        self.assertEqual(result[0]*result[1], 12)
+
+    def test_hash(self):
+        key = []
+        for x in range(256):
+            key.append(x)
+        self.assertEqual(hash(key,''), 'a2582a3a0e66e6e86e3812dcb672a272')
+        self.assertEqual(hash(key,'AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd')
+        self.assertEqual(hash(key,'1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d')
+        self.assertEqual(hash(key,'1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e')
 
 if __name__ == '__main__':
     unittest.main()
