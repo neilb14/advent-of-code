1de5c0afbce2b1812d57f6c64de43ad32cf7b44d
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-12-12 23:17:01 -0700

    solved day 13 part 2 (but bruted it)

--- day13/solution.py
+++ day13/solution.py
@@ -24,20 +24,32 @@ def initialize(input):
     return result
 
 
-def run(input):
+def run(input,delay):
     firewall = initialize(input)
-    count,result = 0,0
+    count,result,caught = delay,0,False
     for layer in firewall:
         #print(f'[{count}] | {layer["depth"]}: {layer["range"]}')
         if(layer['range'] > 0):
             scan = count % (layer['range'] + layer['range']-2)
             if(scan == 0):
+                caught = True
                 #print(f'caught! [{count}] | {layer["depth"]}: {layer["range"]}')
                 result += layer['depth'] * layer['range']
         count += 1
+    if(caught and result <= 0):
+        return 1
     return result
+
+def safe_run(input):
+    delay = 0
+    while(True):
+        #print(f' --- {delay} --- ')
+        if(run(input,delay)==0):
+            return delay
+        delay +=1
 
 if __name__ == '__main__':
     with open(sys.argv[1], "r") as input_file:
         data = input_file.readlines()
-        print(run(data))
+        print(run(data, 0))
+        print(safe_run(data))
--- day13/test.py
+++ day13/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import run
+from solution import run, safe_run
 
 class TestSolution(unittest.TestCase):
 
@@ -10,7 +10,16 @@ class TestSolution(unittest.TestCase):
             '4: 4',
             '6: 4'
         ]
-        self.assertEqual(run(commands), 24)
+        self.assertEqual(run(commands, 0), 24)
+
+    def test_safe_run(self):
+        commands = [
+            '0: 3',
+            '1: 2',
+            '4: 4',
+            '6: 4'
+        ]
+        self.assertEqual(safe_run(commands), 10)
 
 if __name__ == '__main__':
     unittest.main()
