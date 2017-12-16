3ec6b4fbc78046f81f605c72dbf18a3e7163d9a2
Neil Bourgeois <neil.bourgeois@gmail.com>
2017-12-15 22:02:52 -0700

    solved day15 part 2

--- day15/solution.py
+++ day15/solution.py
@@ -9,12 +9,24 @@ class Generator():
         self.value = self.factor*self.value%2147483647
         return self.value
 
+class SafeGenerator():
+    def __init__(self, base, factor, start):
+        self.base = base
+        self.factor = factor
+        self.value = start
+
+    def next(self):
+        self.value = self.factor*self.value%2147483647
+        while(self.value % self.base != 0):
+            self.value = self.factor*self.value%2147483647
+        return self.value
+
 
 
 def run(start_a, start_b, iters):
     count = 0
-    gen_a = Generator(16807, start_a)
-    gen_b = Generator(48271, start_b)
+    gen_a = SafeGenerator(4,16807, start_a)
+    gen_b = SafeGenerator(8,48271, start_b)
     for i in range(iters):
         a = gen_a.next()
         b = gen_b.next()
@@ -23,4 +35,4 @@ def run(start_a, start_b, iters):
     return count
 
 if __name__ == '__main__':
-        print(run(512, 191, 40000000))
+        print(run(512, 191, 5000000))
--- day15/test.py
+++ day15/test.py
@@ -1,5 +1,5 @@
 import unittest
-from solution import run, Generator
+from solution import run, Generator, SafeGenerator
 
 class TestSolution(unittest.TestCase):
 
@@ -10,6 +10,14 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(generator.next(), 245556042)
         self.assertEqual(generator.next(), 1744312007)
         self.assertEqual(generator.next(), 1352636452)
+
+    def test_safe_generator(self):
+        generator = SafeGenerator(4, 16807, 65)
+        self.assertEqual(generator.next(), 1352636452)
+        self.assertEqual(generator.next(), 1992081072)
+        self.assertEqual(generator.next(), 530830436)
+        self.assertEqual(generator.next(), 1980017072)
+        self.assertEqual(generator.next(), 740335192)
 
     def test_run(self):
         self.assertEqual(run(65, 8921, 5), 1)
